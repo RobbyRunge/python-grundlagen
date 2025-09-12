be01a5a518f4ead817a0cb6fff5d52ac2e5582f1
RobbyR98 <robbyrunge@yahoo.com>
2025-09-12 16:06:22 +0200

    feat: add variable and data type exercises in aufgaben.py

---
+++ variablen-und-datentypen/datentypen-übungsaufgaben/aufgaben.py
@@ -0,0 +1,18 @@
+# Übungsaufgaben:
+
+# Welche der folgenden Umwandlungen sind möglich und was ist das Ergebnis?
+  
+int(42.5)     # möglich - 42
+float(-1)     # möglich - -1.0
+str(0.5)      # möglich - "0.5"
+bool(0.001)   # möglich - True
+str("False")  # möglich - "False"
+int(False)    # möglich - 0
+float("10")   # möglich - 10.0 
+bool('0')     # möglich - True
+int("False")  # ValueError
+float("True") # ValueError
+
+# Welche der folgenden Umwandlungen sind möglich und was ist das Ergebnis? 
+
+type()
