602a620a627be307bec95f7dc0b0a23dda64364c
RobbyR98 <robbyrunge@yahoo.com>
2025-09-12 09:44:54 +0200

    feat: add initial implementation of string examples in Python

---
+++ strings/strings.py
@@ -0,0 +1,11 @@
+# Verschiedene Arten von Zeichenketten in Python
+name = "John"
+name1 = 'Doe'
+
+# Ausgabe der Zeichenketten bleibt Struktur erhalten
+print("""Lieber Hacker,
+      
+      Hackers gonna hack 00111010 00101001
+
+      Viele Grüße
+  """)
