676bf9939d9682a26c94398aaf6a4e45719bc8bf
RobbyR98 <robbyrunge@yahoo.com>
2025-09-12 14:18:41 +0200

    refactor: improve comments and add slicing examples in strings.py

--- strings/strings.py
+++ strings/strings.py
@@ -1,7 +1,6 @@
 # Verschiedene Arten von Zeichenketten in Python
 name_test = "John"
 name_test1 = 'Doe'
-
 # Ausgabe der Zeichenketten bleibt Struktur erhalten
 print("""Lieber Hacker,
       
@@ -10,7 +9,7 @@ print("""Lieber Hacker,
       Viele Grüße
   """)
 
-# Länge einer Zeichenkette ermitteln
+# len (Länge) einer Zeichenkette
 name = "Robby"
 
 print(len("Robby"))  # Direkte Ausgabe der Länge
@@ -26,3 +25,14 @@ print("Robby"[-8])  # Zugriff auf ein einzelnes Zeichen von hinten (Index außer
 n = len("Robby")
 print("Robby"[n])  # Zugriff auf ein einzelnes Zeichen in der Zeichenkette (Index außerhalb des Bereichs)
 print("Robby"[-1])  # Zugriff auf das letzte Zeichen in der Zeichenkette
+
+# Slicing (Ausschneiden) von Zeichenketten
+name_slicing = "Robby"
+
+print(name_slicing[0:3])  # Ausgabe der ersten drei Zeichen (Index 0 bis 2), Endindex ist exklusiv (ausschließend) - Ausgabe: Rob
+print(name_slicing[:3])  # Startindex weglassen, wenn von Anfang an geschnitten werden soll - Ausgabe: Rob
+print(name_slicing[2:])  # Endindex weglassen, wenn bis zum Ende geschnitten werden soll - Ausgabe: bby
+
+print(name_slicing[-1000:1000])  # Start- und Endindex außerhalb des Bereichs - Ausgabe: Robby
+
+print(name_slicing[:])  # Start- und Endindex weglassen, um die gesamte Zeichenkette zu erhalten - Ausgabe: Robby
