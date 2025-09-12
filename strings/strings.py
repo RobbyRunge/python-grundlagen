154e3d8f9333d4f7c967ae523e1d52536bbe242a
RobbyR98 <robbyrunge@yahoo.com>
2025-09-12 12:12:45 +0200

    fix: rename variables for consistency and clarity in string examples

--- strings/strings.py
+++ strings/strings.py
@@ -1,6 +1,6 @@
 # Verschiedene Arten von Zeichenketten in Python
-name = "John"
-name1 = 'Doe'
+name_test = "John"
+name_test1 = 'Doe'
 
 # Ausgabe der Zeichenketten bleibt Struktur erhalten
 print("""Lieber Hacker,
@@ -9,3 +9,20 @@ print("""Lieber Hacker,
 
       Viele Grüße
   """)
+
+# Länge einer Zeichenkette ermitteln
+name = "Robby"
+
+print(len("Robby"))  # Direkte Ausgabe der Länge
+print(len(name))  # Ausgabe der Länge der Zeichenkette
+
+print("Robby"[4])  # Zugriff auf ein einzelnes Zeichen in der Zeichenkette
+print("Robby"[-4])  # Zugriff auf ein einzelnes Zeichen von hinten (letzte Position ist -1)
+
+# Fehlerbeispiele
+print("Robby"[8])  # Zugriff auf ein einzelnes Zeichen in der Zeichenkette (Index außerhalb des Bereichs) 
+print("Robby"[-8])  # Zugriff auf ein einzelnes Zeichen von hinten (Index außerhalb des Bereichs)
+
+n = len("Robby")
+print("Robby"[n])  # Zugriff auf ein einzelnes Zeichen in der Zeichenkette (Index außerhalb des Bereichs)
+print("Robby"[-1])  # Zugriff auf das letzte Zeichen in der Zeichenkette
