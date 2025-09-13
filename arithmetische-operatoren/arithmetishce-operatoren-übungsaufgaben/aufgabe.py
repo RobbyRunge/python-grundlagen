092cb805cc85e906f00800d93dfbb6a8ade0c868
RobbyR98 <robbyrunge@yahoo.com>
2025-09-13 14:19:35 +0200

    refactor: add new arithmetic exercises in aufgabe.py

---
+++ arithmetische-operatoren/arithmetishce-operatoren-übungsaufgaben/aufgabe.py
@@ -0,0 +1,27 @@
+# Übungsaufgaben:
+
+# Was kommt bei den folgenden Rechnungen als Ergebnis heraus?
+12 * 12                     # Ausgabe: 144
+2.5 + 3.5                   # Ausgabe: 6
+20 / 5                      # Ausgabe: 4
+15 - 3 * 5                  # Ausgabe: 0
+8 + 10//4                   # Ausgabe: 10                     
+8 + 10/4                    # Ausgabe: 10.5 
+20 / (5 - 4)                # Ausgabe: 20
+4 * 2.5 / 4                 # Ausgabe: 2.5                       
+1//2 - 3//4                 # Ausgabe:  
+(12 % 5) * (5 % 12)         # Ausgabe:                    
+2**3                        # Ausgabe:
+4**(8 -5)                   # Ausgabe:                    
+2**(8 // 2)                 # Ausgabe:                    
+5**2 * 3                    # Ausgabe:
+(8 - 5)**(27 // 9)          # Ausgabe:
+"apfel" + "baum"            # Ausgabe:
+5 * ("a" + "b")             # Ausgabe:                      
+"x" * 2**3                  # Ausgabe:
+12 % 5                      # Ausgabe:
+42 % 43                     # Ausgabe:                    
+2**10 % 2                   # Ausgabe:                    
+5 * 4 * 3 * 2 * 1.0         # Ausgabe:                    
+0 * "Florian"               # Ausgabe:                    
+"xyz" * (12 % 5)            # Ausgabe:
