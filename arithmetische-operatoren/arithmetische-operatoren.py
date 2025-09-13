2ef24531eac4e364799d3d8b1181f4b0a904e1db
RobbyR98 <robbyrunge@yahoo.com>
2025-09-13 14:06:48 +0200

    refactor: add additional examples for arithmetic operations in arithmetische-operatoren.py

--- arithmetische-operatoren/arithmetische-operatoren.py
+++ arithmetische-operatoren/arithmetische-operatoren.py
@@ -19,3 +19,13 @@ print(9 ** 0.5)     # Ausgabe: 3.0    # Exponentiation (Quadratwurzel von 9)
 print(2 + 3 * 4)    # Ausgabe: 14     # Multiplikation hat höhere Priorität als Addition
 print((2 + 3) * 4)  # Ausgabe: 20     # Klammern ändern die Priorität
 print(10 / 2 + 5)   # Ausgabe: 10.0   # Division hat höhere Priorität als Addition (10 : 2 + 5)
+
+# BEISPIELE:
+print(5**3 + 2)         # Ausgabe: 127
+print (4 + 3 - 5)       # Ausgabe: 2
+print(6 + 8 / 2)        # Ausgabe: 10.0
+print(10 * 2**3)        # Ausgabe: 80
+print(5 * 9 *2 / 10)    # Ausgabe: 9.0
+print(5 % 3 * 7)        # Ausgabe: 14
+print(6 % 5 + 3)        # Ausgabe: 4
+print(10 % 2**2)        # Ausgabe: 2
