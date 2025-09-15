7ca7a1b9b053c152a74f9c5add07e3c92f7cd1ae
RobbyR98 <robbyrunge@yahoo.com>
2025-09-15 09:46:37 +0200

    refactor: restore and correct arithmetic exercise outputs in aufgabe.py

--- arithmetische-operatoren/arithmetishce-operatoren-übungsaufgaben/aufgabe.py
+++ arithmetische-operatoren/arithmetishce-operatoren-übungsaufgaben/aufgabe.py
@@ -9,19 +9,19 @@
 8 + 10/4                    # Ausgabe: 10.5 
 20 / (5 - 4)                # Ausgabe: 20
 4 * 2.5 / 4                 # Ausgabe: 2.5                       
-1//2 - 3//4                 # Ausgabe:  
-(12 % 5) * (5 % 12)         # Ausgabe:                    
-2**3                        # Ausgabe:
-4**(8 -5)                   # Ausgabe:                    
-2**(8 // 2)                 # Ausgabe:                    
-5**2 * 3                    # Ausgabe:
-(8 - 5)**(27 // 9)          # Ausgabe:
-"apfel" + "baum"            # Ausgabe:
-5 * ("a" + "b")             # Ausgabe:                      
-"x" * 2**3                  # Ausgabe:
-12 % 5                      # Ausgabe:
-42 % 43                     # Ausgabe:                    
-2**10 % 2                   # Ausgabe:                    
-5 * 4 * 3 * 2 * 1.0         # Ausgabe:                    
-0 * "Florian"               # Ausgabe:                    
-"xyz" * (12 % 5)            # Ausgabe:
+1//2 - 3//4                 # Ausgabe: 0 
+(12 % 5) * (5 % 12)         # Ausgabe: 10                   
+2**3                        # Ausgabe: 8
+4**(8 - 5)                  # Ausgabe: 64                    
+2**(8 // 2)                 # Ausgabe: 16                   
+5**2 * 3                    # Ausgabe: 75
+(8 - 5) ** (27 // 9)        # Ausgabe: 27
+"apfel" + "baum"            # Ausgabe: "apfelbaum"
+5 * ("a" + "b")             # Ausgabe: "ababababab"                     
+"x" * 2**3                  # Ausgabe: "xxxxxxxx"
+12 % 5                      # Ausgabe: 2
+42 % 43                     # Ausgabe: 42                    
+2**10 % 2                   # Ausgabe: 0                     
+5 * 4 * 3 * 2 * 1.0         # Ausgabe: 120.0                   
+0 * "Florian"               # Ausgabe: ""                   
+"xyz" * (12 % 5)            # Ausgabe: "xyzxyz"
