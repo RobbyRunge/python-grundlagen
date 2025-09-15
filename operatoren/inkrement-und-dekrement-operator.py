5eff46bec81f2d24c5e3bcab66e727c4b915809f
RobbyR98 <robbyrunge@yahoo.com>
2025-09-15 10:01:42 +0200

    refactor: add increment and decrement operator examples in inkrement-und-dekrement-operator.py

---
+++ operatoren/inkrement-und-dekrement-operator.py
@@ -0,0 +1,25 @@
+# INKREMENT UND DEKREMENT OPERATOR
+# Python unterstützt keine Inkrement (++) und Dekrement (--) Operatoren wie andere Program
+
+a = 0
+a = a + 1  # Inkrement um 1
+a += 1     # Kurzform für Inkrement um 1
+
+a = a - 1  # Dekrement um 1
+a -= 1     # Kurzform für Dekrement um 1
+
+# BEISPIELE:
+b = 5
+c = 5
+d = 5
+
+b = b / 2
+b /= 2     # Kurzform für b = b / 2
+b //= 2    # Kurzform für b = b // 2 (ganzzahlige Division)
+
+c = c * 3
+c *= 3     # Kurzform für c = c * 3
+c **= 3    # Kurzform für c = c ** 3 (Exponentiation)
+
+d = d % 3
+d %= 3     # Kurzform für d = d % 3 (Modulo)
