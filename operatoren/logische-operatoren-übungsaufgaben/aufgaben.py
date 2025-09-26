9449641a79d447217f592b6aacb3860ae1829531
RobbyR98 <robbyrunge@yahoo.com>
2025-09-26 20:34:36 +0200

    refactor: complete logical expressions exercises in aufgaben.py

--- operatoren/logische-operatoren-übungsaufgaben/aufgaben.py
+++ operatoren/logische-operatoren-übungsaufgaben/aufgaben.py
@@ -1,18 +1,18 @@
 # Übungsaufgaben:
 
 # 1. Welche Wahrheitswerte kommen bei den folgenden logischen Ausdrücken heraus?
-True and False and True or False      # 
-not False or not True                 #
-True and (False or not False)         #
-not (not False ^True or not False)    #
-True and False ^ True and False       #
+True and False and True or False      # False
+not False or not True                 # True
+True and (False or not False)         # True
+not (not False ^ True or not False)   # False
+True and False ^ True and False       # False
 
 # 2. Worin besteht der Unterschied zwischen den beiden Operatoren ^ und or?
-#
+# Bei XOR kann nur einer der beiden True ergeben und bei OR ist es egal ob beide True sind
 
 # 3. Welche Wahrheitswerte kommen als Ergebnis heraus? 
-2 < 3 and not 2 > 5                 #
-not True ^ False or 3 == 2 + 1      #
-not not not 2 % 5 == 7 % 5          #
-True and False ^ True and False     #
-True ^ False ^ 0 ^ 1 ^ (2 > 3)      #
+2 < 3 and not 2 > 5                 # True
+not True ^ False or 3 == 2 + 1      # True
+not not not 2 % 5 == 7 % 5          # False
+True and False ^ True and False     # False
+True ^ False ^ 0 ^ 1 ^ (2 > 3)      # 0
