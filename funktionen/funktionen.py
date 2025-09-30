6b516b2c77ebe777269aadf7d60243c1b9a7bc1f
RobbyR98 <robbyrunge@yahoo.com>
2025-09-30 12:32:15 +0200

    refactor: add comprehensive implementation and examples for function operations in funktionen.py

---
+++ funktionen/funktionen.py
@@ -0,0 +1,23 @@
+# FUNKTIONEN
+# Funktionen sind wiederverwendbare Codeblöcke, die eine bestimmte Aufgabe ausführen.
+
+# GRUNDLAGEN
+
+def sag_hallo1(name):         # Funktionskopf - ":" signalisiert das Ende des Kopfes - es darf keine leere Funktionen geben
+  pass                        # Platzhalter, wenn die Funktion noch keinen Code hat
+
+def sag_hallo2(vorname, nachname):          # Funktionskopf
+  print(f"Hallo {vorname} {nachname}")      # Funktionskörper
+
+sag_hallo2("Max", "Mustermann")             # Funktionsaufruf, Ausgabe: Hallo Max Mustermann
+
+
+def addieren(a, b):
+  summe = a + b
+  return summe            # Rückgabewert der Funktion (Terminal von VSCode zeigt an, dass die Funktion einen Wert zurückgibt, in dem Fall 42. Wird aber nicht angezeigt im normalen Terminal)
+
+addieren(10, 32)
+print(addieren(10, 32))      # Ausgabe: 42 - Rückgabewert wird ausgegeben
+
+s = addieren(10, 32)         # Rückgabewert wird in Variable gespeichert (s hat jetzt den Wert 42)
+print(f"Summe: {s}")         # Ausgabe: Summe: 42
