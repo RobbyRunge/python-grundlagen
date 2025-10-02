e12d76126b1f6b8fbcfd5b6a56df170f34f62918
RobbyR98 <robbyrunge@yahoo.com>
2025-10-02 14:55:03 +0200

    feat: add Pokemon class with constructor, vorstellen, and getter for level

--- objektorientierung/einführung.py
+++ objektorientierung/einführung.py
@@ -2,7 +2,7 @@
 # Klassen sind Baupläne für Objekte
 
 class Mammal:                                             # Definiere die Klasse "Mammal" - Säugetier (Eigenschaften, was mehrere Objekte gemeinsam haben werden vererbt)
-  def __init__(self, farbe, rasse, name):                 # Konstruktor-Methode, wird aufgerufen, wenn ein neues Objekt der Klasse erstellt wird
+  def __init__(self, farbe, rasse, name):                 # Initialisierungsmethode (oft als "Konstruktor" bezeichnet), wird aufgerufen, wenn ein neues Objekt der Klasse erstellt wird. Das self ist ein Verweis auf das aktuelle Objekt (die Instanz) der Klasse.
     self.farbe = farbe                                    # Instanzvariable "farbe"
     self.rasse = rasse                                    # Instanzvariable "rasse"
     self.name = name                                      # Instanzvariable "name"
@@ -35,3 +35,26 @@ dog1.vorstellen()
 dog2.vorstellen()
 cat1.vorstellen()
 cat2.vorstellen()
+
+# ----------------------------------------
+
+class Pokemon:
+  def __init__(self,name, level):
+    self.name = name
+    self.__level = level                     # Private Variable, nicht direkt von außen zugreifbar
+    self.vorstellen()                        # Aufruf der Methode innerhalb des Konstruktors (self muss angegeben werden, da es sich um eine Instanzmethode handelt) - macht die unteren Zeilen überflüssig 1*
+
+  def vorstellen(self):
+    print(f"{self.name}, {self.name}!")
+
+  def get_level(self):                       # Getter-Methode
+    return self.__level                      # Zugriff auf die private Variable
+
+if __name__ == "__main__":
+  bisasam = Pokemon("Bisasam", 5)            # Instanzierung eines Objekts der Klasse Pokemon Reihenfolge beachtet, davor kann nicht die bisasam.vorstellen() Methode aufgerufen werden
+  schiggy = Pokemon("Schiggy", 8)
+  
+  print(bisasam.get_level())                 # Zugriff auf die private Variable über die Getter-Methode
+
+  # schiggy.vorstellen()                     # 1*
+  # bisasam.vorstellen()                     # 1*        
